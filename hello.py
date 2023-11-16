b76ae81deb63fa87d2a5f7b95e8f66b39cd05ba7
Julian <gomezjulian51998@gmail.com>
2023-11-16 14:57:30 -0500

    Hello python
    Ver tipos de datos y comentarios

---
+++ hello.py
@@ -0,0 +1,13 @@
+#Comentarios en una sola linea
+"""
+Comentarios
+en varias
+lineas
+"""
+# Cómo consultar el tipo de dato
+print(tye("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(1.5))  # Tipo 'float'
+print(type(3 + 1j))  # Tipo 'complex'
+print(type(True))  # Tipo 'bool'
+print(type(print("Mi cadena de texto")))  # Tipo 'NoneType'
